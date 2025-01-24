d7321836c639215d9944e1a58fa342adddf2bf96
Abhinav singh <asingh308@nait.ca>
2025-01-23 19:37:47 -0700

    enhanced the command line code interface

--- taskManagementSystem/src/cli/command_handler.py
+++ taskManagementSystem/src/cli/command_handler.py
@@ -8,5 +8,17 @@ class CommandHandler:
 
     def setup_commands(self):
         parser = argparse.ArgumentParser(description='Task Manager')
-        parser.add_argument('command', choices=['add', 'list', 'complete'])
+        subparser = parser.add_subparsers(dest="command",help="Available commands")
+
+        add_parser = subparser.add_parser('add',help="Add a new Task")
+        add_parser.add_argument('title',help='Task tile')
+        add_parser.add_argument('description', help='Task description')
+        add_parser.add_argument('priority',type=int,choices=[1,2,3], help='Priority (1=High,2=Medium,3=Low)')
+        list_parser= subparser.add_parser('list', help = 'List all tasks')
+        list_parser.add_argument('--status', choices=['pending','completed'],
+                                 help='Filter tasks by status')
+
+        complete_parser = subparser.add_parser('complete', help='Mark a task as completed')
+        complete_parser.add_argument('task_id',type=int,help='ID of the task to complete')
+
         return parser
--- taskManagementSystem/src/main.py
+++ taskManagementSystem/src/main.py
@@ -1,23 +1,49 @@
 from dataclasses import dataclass
 from datetime import datetime
 from models.task import Task
+from cli.command_handler import CommandHandler
+from context.db_connection import DatabaseHandler
 
 
 def main():
-    task = Task(
-        title="Complete Python Project",
-        description="Build a task management system",
-        priority=1,
-        due_date=datetime.now(),
+    handler = CommandHandler()
+    parser = handler.setup_commands()
+    args = parser.parse_args()
 
-    )
+    if not args.command:
+        parser.print_help()
+        return
 
-    print(f"Task Created :{task.title}")
-    print(f"Task Description:{task.description}")
-    print(f"Status:{task.status}")
+    if args.command == "add":
+        task = Task(
+            title=args.title,
+            description=args.description,
+            priority=args.priority,
+            due_date=datetime.now()
+        )
+        print(f"Added task: {task.title}")
 
-    task.mark_completed()
-    print(f"Status after completion : {task.status}")
+    elif args.command == "list":
+        print("Listing tasks...")
+
+    elif args.command == "complete":
+
+        print(f"Marking task {args.task_id} as completed...")
+
+    # task = Task(
+    #     title="Complete Python Project",
+    #     description="Build a task management system",
+    #     priority=1,
+    #     due_date=datetime.now(),
+    #
+    # )
+    #
+    # print(f"Task Created :{task.title}")
+    # print(f"Task Description:{task.description}")
+    # print(f"Status:{task.status}")
+    #
+    # task.mark_completed()
+    # print(f"Status after completion : {task.status}")
 
 
 if __name__ == "__main__":
