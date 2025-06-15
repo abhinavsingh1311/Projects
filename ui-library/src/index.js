1220fca3a11e98cf2a2f7a7f09e3653f0fb1bbe6
Abhinav singh <asingh308@nait.ca>
2025-06-15 15:50:27 -0600

    UI library completed

--- ui-library/src/index.js
+++ ui-library/src/index.js
@@ -1,7 +1,29 @@
 import Tooltip from "./ninja-ui/tooltip";
-
+import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 //create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dd => {
+    const instance = new Dropdown(dd);
+    instance.init();
+});
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+
+tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('clicked!!');
+});
+
 
